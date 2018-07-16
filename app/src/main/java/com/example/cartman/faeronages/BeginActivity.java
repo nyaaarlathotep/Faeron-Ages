a5e50e8461d478a21ac75db193ef0579464eb601
nyaaarlathotep <37174794+nyaaarlathotep@users.noreply.github.com>
2018-07-16 18:22:00 +0800

    awo

--- app/src/main/java/com/example/cartman/faeronages/BeginActivity.java
+++ app/src/main/java/com/example/cartman/faeronages/BeginActivity.java
@@ -1,17 +1,18 @@
 package com.example.cartman.faeronages;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class BeginActivity extends AppCompatActivity {
+public class BeginActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_begin);
+
+
         Button buttonStart =(Button) findViewById(R.id.buttonStart);
         Button buttonOptions=(Button)findViewById(R.id.buttonOptions);
         Button buttonQuit=(Button)findViewById(R.id.buttonQuit);
@@ -31,9 +32,9 @@ public class BeginActivity extends AppCompatActivity {
         });
         buttonQuit.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v){
-                Intent intent=new Intent(BeginActivity.this,Roll.class);
-                startActivity(intent);
+                ActivityCollector.finishAll();
             }
         });
     }
+
 }
