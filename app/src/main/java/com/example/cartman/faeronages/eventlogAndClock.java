612ea874ecd8cc1e0cf31b13b9811369a426faf2
nyaaarlathotep <37174794+nyaaarlathotep@users.noreply.github.com>
2018-08-03 15:16:33 +0800

    equipment character bag activities established

--- app/src/main/java/com/example/cartman/faeronages/eventlogAndClock.java
+++ app/src/main/java/com/example/cartman/faeronages/eventlogAndClock.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
-import android.view.ActionMode;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
