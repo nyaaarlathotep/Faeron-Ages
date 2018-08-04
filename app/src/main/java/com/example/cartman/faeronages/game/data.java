89a11752b1b0165711af13470636809ac9190b97
nyaaarlathotep <37174794+nyaaarlathotep@users.noreply.github.com>
2018-08-04 16:48:43 +0800

    www

--- app/src/main/java/com/example/cartman/faeronages/game/data.java
+++ app/src/main/java/com/example/cartman/faeronages/game/data.java
@@ -32,13 +32,13 @@ public class data {
             ,"滩羊 羊皮","狮鹫 厚重羽毛","毒蛇 毒腺","野狗 狗牙","小豹子 豹子皮","成年雄狮 狮皮","田鼠 鼠皮","被诅咒的稻草人 诅咒印记"};
     public static String home="may be it's time to go home";
 
-    public static String burd0="舞光术（Dancing Lights）:制造火把或其他光源。 晕眩术（Daze）:4HD 以下的人形生物会失去下一次的动作。侦测魔法（Detect Magic）:"+
+    public static String[] spell0=("舞光术（Dancing Lights）:制造火把或其他光源。 晕眩术（Daze）:4HD 以下的人形生物会失去下一次的动作。侦测魔法（Detect Magic）:"+
             "侦测 60 尺内的法术或魔法物品。闪光术（Flare）:使一个生物目眩（攻击检定-1）。幻音术（Ghost Sound）:发出虚幻的假声。指北术（Know Direction）:指" +
             "出北方。光亮术（Light）:使目标物品如火把般发光。瞌睡术（Lullaby）:使受术者昏昏欲睡，「侦察」与「聆听」检定受到-5 减值，对抗「睡眠术」时的意志检定受到-2 减值。" +
             "法师帮手（Mage Hand）:可对 5磅以内的物品使用心灵遥控。修复术（Mending）:修复目标物品的细微损伤。传讯术（Message）:可在远距离使用轻声交谈。" +
             "开关术（Open/Close）:打开或关上小或轻的目标物品。魔法技俩（Preestidigitation）:玩一些小把戏。阅读魔法（Read Magic）:阅读卷轴及法术书。" +
-            "提升抗力（Resistance）:受术者的豁免检定获获得+1 加值。召唤乐器（Summon Instrument）:召唤出一项施法者指定的乐器。";
-    public static String burd1="魔法警报（Alarm）:产生警报结界，持续时间为每等级 2 小时。 活化绳（Animate Rope）:造出一条魔法绳，依你的命令动作。惊恐术（Cause F"+
+            "提升抗力（Resistance）:受术者的豁免检定获获得+1 加值。召唤乐器（Summon Instrument）:召唤出一项施法者指定的乐器。").split("。");
+    public static String[] spell1=("魔法警报（Alarm）:产生警报结界，持续时间为每等级 2 小时。 活化绳（Animate Rope）:造出一条魔法绳，依你的命令动作。惊恐术（Cause F"+
             "ear）:使一个5HD 以下的生物逃窜 1d4 个回合。魅惑人类（Charm Person）:使一个人类成为你的朋友。通晓语言（ComprehendLanguages）:懂得所有可说可写的语言。" +
             "治疗轻伤（Cure Light Wounds）:治疗 1d8 点伤害，每等级再多治疗+1 点，最高+5 点。侦测密门（Detect SecretDoors）:发现 60 尺内的密门。" +
             "魔法易容（Disguise Self）:改变自己的外貌。抹消术（Erase）:消除一般或魔法性的书写文字。脚底抹油（ExpeditiousRetreat）:你的速度增加 30尺。" +
@@ -47,22 +47,22 @@ public class data {
             "鉴定术（Identify） M ：鉴识魔法物品的其中一项特性。次级困惑术（LesserConfusion）:使受术者受到困惑，持续时间为 1 轮。" +
             "魔嘴（Magic Mouth ）M:触发时会说话。涅斯图伪装灵光（Magic Aura）:改变物品的灵光。遮蔽物品（Obscure Object）:目标物品不会被探测到。" +
             "移除恐惧（Remove Fear）:压抑恐惧感，或使一个受术者的抗恐惧豁免检定+4，每 4 等级增加一对象。无声幻影（Silent Image）:造出你想要的幻影。" +
-            "睡眠术（Sleep）:使生命骰数和为 4 的生物群陷入魔法昏睡。";
-    public static String burd2="变身术（Alter Self）:变为类似的生物。动物使者（Animal Messenger）:将一只超小型动物送至指定地点。" +
+            "睡眠术（Sleep）:使生命骰数和为 4 的生物群陷入魔法昏睡。").split("。");
+    public static String[] spell2=("变身术（Alter Self）:变为类似的生物。动物使者（Animal Messenger）:将一只超小型动物送至指定地点。" +
             "迷惑动物（Animal Trance）:使生命骰数和为 2d6 的动物陷入迷魂状态。目盲术／耳聋术（BlindnessDeafness）:使受术者目盲或耳聋。" +
             "朦胧术（Blur）:对受术者的攻击会有 20%的失手几率。安定心神（Calm Emotions）:安抚生物，抵销情绪性效应。" +
             "轻灵术（Cat‘s Grace）:受术者获得+4 敏捷加值，持续时间为每等级 1 分钟。治疗中度伤（Cure ModerateWounds）:治疗 2d8 点伤害，每等级再多治疗+1 点，最高+10点。" +
             "黑暗术（Darkness）:产生半径20 尺的超自然黑暗。怪物晕眩术（DazeMonster）:6HD 以下的活物会失去下一次的动作。" +
-            "减缓毒性（Delay Poison）:使毒性停止对受术者造成伤害，每等级持续 1 小时。侦测思想（Detect Thoughts）:可以观察到他人的表面想法。";
-    public static String burd3="闪现术（Blink）：你随机的消失、出现，持续时间 1 轮/每等级。魅惑怪物（Charm Monster）：使怪物相信它是你的盟友。" +
+            "减缓毒性（Delay Poison）:使毒性停止对受术者造成伤害，每等级持续 1 小时。侦测思想（Detect Thoughts）:可以观察到他人的表面想法。").split("。");
+    public static String[] spell3=("闪现术（Blink）：你随机的消失、出现，持续时间 1 轮/每等级。魅惑怪物（Charm Monster）：使怪物相信它是你的盟友。" +
             "锐耳术/鹰眼术（Clairaudience/Clairvoyance）：听或看到更远的地方，持续时间 1 分钟/每等级。困惑术（Confusion）：受术者行为古怪，持续时间 1 轮/每等级。" +
             "满怀绝望（Crushing Despair）：目标攻击掷骰、伤害掷骰、豁免，以及其他所有检定均–2。治疗重伤（Cure Serious Wounds）：治疗 3d8 点伤害，每等级再多治疗 1 点，最多+15。" +
             "昼明术（Daylight）：产生半径60 尺范围的亮光。沉睡术（Deep Slumber）：使生命骰总数 10 的生物沉睡。解除魔法（Dispel Magic）：解除魔法或魔法效果。" +
             "移位术（Displacement）：攻击受术者有 50%的失手几率。恐惧术（Fear）：锥形区域内的目标群逃窜，持续时间 1 轮/每等级。气化形体（Gaseous Form）：受术者变为一团气体并能缓慢飞行。" +
             "次等指使术（Geas, Lesser）：指挥生命骰为 7 或更少的生物。舌灿莲花:「唬骗」检定获得+30加值，且谎言不会被魔法拆穿。"+
             "满怀希望（Good Hope）：目标攻击掷骰、伤害掷骰、豁免，以及所有检定均+2。加速术（Haste）：受术者移动速度加快，攻击掷骰、 AC、反射豁免检定各+1。每等级影响 1个生物。" +
-            "谜幻手稿（Illusory Script ） M:只有事先指定的读者可解读此文字。隐形法球（InvisibilitySphere）:使 10 尺内所有人隐形。 ";
-    public static String burd4="破除结界（Break Enchantment）:\n" +
+            "谜幻手稿（Illusory Script ） M:只有事先指定的读者可解读此文字。隐形法球（InvisibilitySphere）:使 10 尺内所有人隐形。").split("。");
+    public static String[] spell4=resolveSpell("破除结界（Break Enchantment）:\n" +
             "破除受术者所带有的附加魔\n" +
             "法、诅咒、石化，或解除其变\n" +
             "化。 \n" +
@@ -96,7 +96,49 @@ public class data {
             "关传说。 \n" +
             "李欧蒙庇护所\n" +
             "（Leomund’sSecureShelter）:\n" +
-            "造出坚固的屋舍。 ";
+            "造出坚固的屋舍。 ");
+    public static String[] spell5=resolveSpell("集体治疗轻伤（Cure Light \n" +
+            "Wounds, Mass）:对多个生物治\n" +
+            "疗 1d8 点伤害，每等级再多治\n" +
+            "疗+1 点。 \n" +
+            "高等解除魔法\n" +
+            "（DispelMagic,Greater）:与\n" +
+            "「解除魔法」同，但检定+20。 \n" +
+            "托梦法（Dream）:将讯息送至\n" +
+            "任何正在睡眠中的人。 \n" +
+            "梦魇（Nightmare）:送出虚像，\n" +
+            "可造成 1d10 点伤害，并使对象\n" +
+            "疲乏。 \n" +
+            "常驻幻影（Persistent Image）:\n" +
+            "与「高等幻影」同，但不需集\n" +
+            "中精神。 \n" +
+            "Evocation）:假拟低于五级的\n" +
+            "塑能系法术，但只有 20%的真实\n" +
+            "性。 \n" +
+            "集体暗示（Suggestion, Mass）:\n" +
+            "与「暗示」同，但可影响的生\n" +
+            "物个数与等级同。 \n" +
+            "五级召唤怪物术（Summon \n" +
+            "Monster V）:可召唤外界生物\n" +
+            "为你作战。 \n" );
+    public static String[] spell6=resolveSpell("解析咒文（Analyze Dweomer）\n" +
+            ":发现受术者的魔法形态。 \n" +
+            "活化物体（Animate Objects）:\n" +
+            "目标物品会攻击你的对手。 \n" +
+            "集体轻灵术（Cat‘s Grace, \n" +
+            "Mass）:与「轻灵术」同，但可\n" +
+            "影响的生物个数与等级同。 \n" +
+            "集体魅惑怪物（Charm Monster, \n" +
+            "Mass）:与「魅惑怪物」同，但\n" +
+            "范围为 30 尺。 \n" +
+            "集体治疗中度伤（Cure \n" +
+            "Moderate Wounds, Mass）:对\n" +
+            "多个生物治疗 2d8 点伤害，每\n" +
+            "等级再多治疗+1 点。 \n" +
+            "集体耀眼术（Eagle‘s Splendor, \n" +
+            "Mass）:与「耀眼术」同，但可\n" +
+            "影响的生物个数与等级同。 ");
+
 
 
     private static String[] resolveSpell(String sad){
